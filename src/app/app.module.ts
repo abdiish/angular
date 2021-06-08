12d643d699fd0dd0eac8e4ebb674e2083950550a
Alan Cortes <ing.ti.upmh@gmail.com>
2021-06-08 12:34:57 -0500

    Se crearon los componentes contact y about, usar el comando ng g c pages/about --dry-run solo para visualizar como se estaran creando los archivos.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
