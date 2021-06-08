ed50f0b613c4e710794c1bedbe5df487dc69fe91
Alan Cortes <ing.ti.upmh@gmail.com>
2021-06-08 12:45:15 -0500

    Se creó el componente home y se inyectaron con ayuda de el selector en app.component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
